e7a42cfae6b1747a915fe1bc5278e5ceb02ed444
antomfdez <antonio.mesa.fdez@gmail.com>
2024-11-28 23:34:44 +0100

    Cleaning code

--- config.hs
+++ config.hs
@@ -38,10 +38,7 @@ myModMask :: KeyMask
 myModMask       = mod1Mask
 
 myWorkspaces :: [String]
-myWorkspaces    = ["web", "code", "ctrl", "term", "doc", "virt", "test", "files", "other" ] -- ++ map show [5..9]
-
-
--- ["1","2","3","4","5","6","7","8","9"]
+myWorkspaces    = ["web", "code", "ctrl", "term", "doc", "virt", "test", "files", "other" ]
 
 myNormalBorderColor :: String
 myNormalBorderColor  = "#000000"
@@ -59,6 +56,7 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
     [ ((modm .|. shiftMask, xK_Return), spawn $ XMonad.terminal conf)
 
     , ((modm,               xK_p     ), spawn "dmenu_run")
+
     , ((modm,               xK_f     ), spawn "flameshot gui")
 
     , ((modm .|. shiftMask, xK_c     ), kill)
@@ -98,8 +96,6 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
     , ((modm .|. shiftMask, xK_q     ), io exitSuccess)
 
     , ((modm              , xK_q     ), spawn "rm -rf ~/.xmonad/dist-newstyle; xmonad --recompile; pkill xmobar; ~/.xmonad/xmonad-x86_64-freebsd --restart")
-
-    , ((modm .|. shiftMask, xK_slash ), xmessage help)
     ]
     ++
     [((m .|. modm, k), windows $ f i)
@@ -183,7 +179,6 @@ myStartupHook = do
 
 myPPminimal = def
    { ppLayout = const ""  -- Don't show the layout name
-   -- , ppSort = getSortByXineramaRule  -- Sort left/right screens on the left, non-empty workspaces after those
    , ppTitle = const ""  -- Don't show the focused window's title
    , ppTitleSanitize = const ""  -- Also about window's title
    , ppCurrent = xmobarColor "#fffff0" "" . wrap "[ " " ]"
@@ -200,7 +195,10 @@ myPPminimal = def
 -- ################################
 
 mySB = statusBarProp "xmobar -x 0 ~/.xmonad/xmobar/xmobarrc" (pure myPPminimal)
-main = xmonad $ toggleFullFloatEwmhFullscreen . ewmhFullscreen . ewmh $ withEasySB mySB defToggleStrutsKey defaults
+main = do
+  xmonad
+  $ toggleFullFloatEwmhFullscreen . ewmhFullscreen . ewmh
+  $ withEasySB mySB defToggleStrutsKey defaults
 
 -- ################################
 -- ||                            ||
@@ -230,54 +228,3 @@ defaults = def {
         logHook            = myLogHook,
         startupHook        = myStartupHook
     }
-
-help :: String
-help = unlines ["The default modifier key is 'alt'. Default keybindings:",
-    "",
-    "-- launching and killing programs",
-    "mod-Shift-Enter  Launch xterminal",
-    "mod-p            Launch dmenu",
-    "mod-Shift-p      Launch gmrun",
-    "mod-Shift-c      Close/kill the focused window",
-    "mod-Space        Rotate through the available layout algorithms",
-    "mod-Shift-Space  Reset the layouts on the current workSpace to default",
-    "mod-n            Resize/refresh viewed windows to the correct size",
-    "mod-Shift-/      Show this help message with the default keybindings",
-    "",
-    "-- move focus up or down the window stack",
-    "mod-Tab        Move focus to the next window",
-    "mod-Shift-Tab  Move focus to the previous window",
-    "mod-j          Move focus to the next window",
-    "mod-k          Move focus to the previous window",
-    "mod-m          Move focus to the master window",
-    "",
-    "-- modifying the window order",
-    "mod-Return   Swap the focused window and the master window",
-    "mod-Shift-j  Swap the focused window with the next window",
-    "mod-Shift-k  Swap the focused window with the previous window",
-    "",
-    "-- resizing the master/slave ratio",
-    "mod-h  Shrink the master area",
-    "mod-l  Expand the master area",
-    "",
-    "-- floating layer support",
-    "mod-t  Push window back into tiling; unfloat and re-tile it",
-    "",
-    "-- increase or decrease number of windows in the master area",
-    "mod-comma  (mod-,)   Increment the number of windows in the master area",
-    "mod-period (mod-.)   Deincrement the number of windows in the master area",
-    "",
-    "-- quit, or restart",
-    "mod-Shift-q  Quit xmonad",
-    "mod-q        Restart xmonad",
-    "mod-[1..9]   Switch to workSpace N",
-    "",
-    "-- Workspaces & screens",
-    "mod-Shift-[1..9]   Move client to workspace N",
-    "mod-{w,e,r}        Switch to physical/Xinerama screens 1, 2, or 3",
-    "mod-Shift-{w,e,r}  Move client to screen 1, 2, or 3",
-    "",
-    "-- Mouse bindings: default actions bound to mouse events",
-    "mod-button1  Set the window to floating mode and move by dragging",
-    "mod-button2  Raise the window to the top of the stack",
-    "mod-button3  Set the window to floating mode and resize by dragging"]
